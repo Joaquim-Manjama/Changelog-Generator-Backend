269a4d8a884a0b1233d5e5d0577ef10a21d6607f
Joaquim Manjama <melorafaelmanjama@gmail.com>
2026-02-06 19:48:30 +0000

    Hello World!

--- src/main/java/JoaquimManjama/ChangelogGenerator/ChangelogGeneratorApplication.java
+++ src/main/java/JoaquimManjama/ChangelogGenerator/ChangelogGeneratorApplication.java
@@ -2,12 +2,20 @@ package JoaquimManjama.ChangelogGenerator;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class ChangelogGeneratorApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ChangelogGeneratorApplication.class, args);
 	}
 
+    @GetMapping
+    public static String helloWorld() {
+        return "Hello World!";
+    }
 }
