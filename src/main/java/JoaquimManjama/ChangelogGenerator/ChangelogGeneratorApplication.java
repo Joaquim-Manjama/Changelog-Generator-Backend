2308b6306d9fe7c3268166d181d21f8e45e9a1bb
Joaquim Manjama <melorafaelmanjama@gmail.com>
2026-02-06 19:17:07 +0000

    first commit

---
+++ src/main/java/JoaquimManjama/ChangelogGenerator/ChangelogGeneratorApplication.java
@@ -0,0 +1,13 @@
+package JoaquimManjama.ChangelogGenerator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChangelogGeneratorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChangelogGeneratorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/JoaquimManjama/ChangelogGenerator/ChangelogGeneratorApplicationTests.java
@@ -0,0 +1,13 @@
+package JoaquimManjama.ChangelogGenerator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChangelogGeneratorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
