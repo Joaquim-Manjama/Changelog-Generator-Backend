4fbeeef012bcbc2a91d2b7bab87c7807bdf845dd
Joaquim Manjama <melorafaelmanjama@gmail.com>
2026-04-13 21:18:21 +0100

    implented exchange of code for access token

--- src/main/java/JoaquimManjama/ChangelogGenerator/Services/GitHubService.java
+++ src/main/java/JoaquimManjama/ChangelogGenerator/Services/GitHubService.java
@@ -1,16 +1,25 @@
 package JoaquimManjama.ChangelogGenerator.Services;
 
 import JoaquimManjama.ChangelogGenerator.Config.GitHubConfig;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @Service
 public class GitHubService {
 
     private final GitHubConfig gitHubConfig;
 
-    public GitHubService(GitHubConfig gitHubConfig) {
+    private final RestTemplate restTemplate;
+
+    public GitHubService(GitHubConfig gitHubConfig,  RestTemplate restTemplate) {
         this.gitHubConfig = gitHubConfig;
+        this.restTemplate = restTemplate;
     }
 
     // Generate the URL to redirect user to GitHub
@@ -23,11 +32,31 @@ public class GitHubService {
 
     // Exchange code for access token
     public String getAccessToken(String code) {
+        String tokenUrl = "https://github.com/login/oauth/access_token";
 
+        // Prepare request body
+        Map<String, String> params = new HashMap<>();
+        params.put("client_id", gitHubConfig.getClientId());
+        params.put("client_secret", gitHubConfig.getClientSecret());
+        params.put("code", code);
+        params.put("redirect_uri", gitHubConfig.getRedirectUri());
+
+        // Set headers
+        HttpHeaders headers = new HttpHeaders();
+        headers.set("Accept", "application/json");
+
+        HttpEntity<Map<String, String>> request = new HttpEntity<>(params, headers);
+
+        // Make POST request
+        ResponseEntity<Map> response = restTemplate.postForEntity(tokenUrl, request, Map.class);
+
+        // Extract access token from response
+        Map<String, Object> body = response.getBody();
+        return (String) body.get("access_token");
     }
 
     // Get user's Github username
     public String getGitHubUsername(String accessToken) {
-
+        return "";
     }
 }
